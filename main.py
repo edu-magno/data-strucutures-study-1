fdb33a9f53c9c6f6cb19f62b6fb54bc448f40817
eduardo <eduardo.magno.u@gmail.com>
2020-11-04 20:39:47 -0300

    feat: call main()

---
+++ main.py
@@ -0,0 +1,9 @@
+from processors.superheros import SUPER_HEROS
+from processors.super_trump_functions import *
+
+def main():
+  play(SUPER_HEROS,(0,0))
+
+
+if __name__ == "__main__":
+  main()
--- processors/super_trump_functions.py
+++ processors/super_trump_functions.py
@@ -1,23 +1,19 @@
-from superheros import SUPER_HEROS
 import random
+
 
 def get_players_deck(card_list):
     half_deck = int(len(card_list) / 2)
-    deck_len = len(card_list) 
-    
+    deck_len = len(card_list)
+
     deck_initial = 0
     if len(card_list) % 2 != 0:
         return None
-    
-    random.shuffle(card_list)
-    
-    player_a = card_list[deck_initial: half_deck]
-    player_b = card_list[half_deck: deck_len ]
-    return (player_a, player_b,)
 
-t = get_players_deck(SUPER_HEROS)
-a = t[0]
-b = t[1]
+    random.shuffle(card_list)
+
+    player_a = card_list[deck_initial: half_deck]
+    player_b = card_list[half_deck: deck_len]
+    return (player_a, player_b,)
 
 
 def compare_cards(card_of_player_a, card_of_player_b, score):
@@ -26,10 +22,9 @@ def compare_cards(card_of_player_a, card_of_player_b, score):
 
     name_card_a = card_of_player_a.get('name')
     name_card_b = card_of_player_b.get('name')
-    
 
     for (key_a, value_a), (key_b, value_b) in zip(card_of_player_a.items(), card_of_player_b.items()):
-        
+
         if key_a != 'name' and key_b != 'name':
             if value_a > value_b:
                 print(f'{name_card_a} have more {key_a}')
@@ -41,17 +36,37 @@ def compare_cards(card_of_player_a, card_of_player_b, score):
                 print(f'{name_card_a} and {name_card_b} have the same {key_a}')
                 continue
     # for x in range(len(card_of_player_a)):
-        
+
     if count_a > count_b:
-        score = (1,0)
+        score = (1, 0)
         return score
     if count_a < count_b:
-        score = (0,1)
+        score = (0, 1)
         return score
-    
+
     return score
 
-compare = compare_cards({'name': 'dr. strange', 'intelligence': 11, 'power': 10, 'strenght': 6, 'agility': 5, 'vitality': 8},{'name': 'beast', 'intelligence': 10, 'power': 9, 'strenght': 6, 'agility': 5, 'vitality': 10},(0,0))
-print(compare)
+
 def play(card_list, score):
-    pass
+    shuffle_cards = get_players_deck(card_list)
+    count_win_a = 0
+    count_win_b = 0
+    deck_player_a = shuffle_cards[0]
+    deck_player_b = shuffle_cards[1]
+
+    for card in range(len(deck_player_a)):
+        result_of_card_battle = compare_cards(
+            deck_player_a[card], deck_player_b[card], (0, 0))
+        if result_of_card_battle == (1, 0):
+            count_win_a += 1
+        if result_of_card_battle == (0, 1):
+            count_win_b += 1
+
+    if count_win_a > count_win_b:
+        print('Player A Win')
+    if count_win_a < count_win_b:
+        print('Player B win')
+    if count_win_a == count_win_b:
+        print('Draw')
+    score = (count_win_a, count_win_b)
+    return score
